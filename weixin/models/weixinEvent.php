23dfd731dedbf9371b3acdaf61c3b7e4877879a9
= <=>
2014-08-31 18:41:25 +0800

    add new dir and file

---
+++ weixin/models/weixinEvent.php
@@ -0,0 +1,33 @@
+<?php
+class wexinEvent {
+
+private $createTime;//创建时间
+private $fromUser;//来源openid
+private $toUser;//目标openid
+private $event;//事件类型
+
+    //construct function 
+    public __construct($ct, $fu, $tu, $e){
+        $this ->$createTime = $ct;
+        $this ->$fromUser = $fu;
+        $this ->$toUser = $tu;
+        $this ->$event = $e;
+    }
+    //insert the event to db 
+    public function insertEvent(){
+        //inset sql ;
+        $sql = 'insert into'. $dbname.'.'.$tablename.'' ;
+        return $this->dbResult($sql);
+    }
+    public function getCreateTime(){
+        return $this->$createTime;
+    }
+    public function setCreateTime($createTime){
+        $this->$createTime = $createTime;
+    }
+    //执行db操作，返回结果
+    private function dbResult($sql){
+    
+    }
+}
+?>
