de1f7a4281f7acf2396564634e61c32cec6cd739
winterswang <paradox0504@gmail.com>
2014-09-01 23:44:03 +0800

    add some codes

--- weixin/models/weixinEvent.php
+++ weixin/models/weixinEvent.php
@@ -7,24 +7,24 @@ private $fromUser;//来源openid
 private $toUser;//目标openid
 private $event;//事件类型
 
-    //construct function 
-    public __construct($ct, $fu, $tu, $e){
-        $this ->$createTime = $ct;
-        $this ->$fromUser = $fu;
-        $this ->$toUser = $tu;
-        $this ->$event = $e;
+    //construct function
+    function __construct($ct, $fu, $tu, $e){
+        $this ->createTime = $ct;
+        $this ->fromUser = $fu;
+        $this ->toUser = $tu;
+        $this ->event = $e;
     }
-    //insert the event to db 
+    //insert the event to db
     public function insertEvent(){
         //inset sql ;
-        $sql = 'insert into'. $this->$dbname.'.'.$this->$tablename.'' ;
+        $sql = 'insert into'. $this->dbname.'.'.$this->tablename.'' ;
         return $this->dbResult($sql);
     }
     public function getCreateTime(){
-        return $this->$createTime;
+        return $this->createTime;
     }
     public function setCreateTime($createTime){
-        $this->$createTime = $createTime;
+        $this->createTime = $createTime;
     }
     //执行db操作，返回结果
     private function dbResult($sql){
