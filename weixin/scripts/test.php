609e469c93b68b889fe77575c6676b0b87855849
winterswang <paradox0504@gmail.com>
2014-09-02 20:01:26 +0800

    update funciton

--- weixin/scripts/test.php
+++ weixin/scripts/test.php
@@ -7,12 +7,13 @@
 define('__ROOT__', dirname(dirname(__FILE__)));
 require_once(__ROOT__.'/models/weixinEvent.php');
 test();
+/**
+* @method test for insert into db
+*/
 function test(){
 	//use of error_log function
-	error_log(__METHOD__.' this is a test');
-	$we = new wexinEvent('1111','222','汉子行嘛？','333');
+	$we = new wexinEvent('1111','222','汉子行嘛？','subscribe');
 	$we ->insertEvent();
 	//echo $we->getCreateTime();
-
 }
 ?>
