0727034f7f96d12027454cd3f1bf42e3fd163ffa
= <=>
2014-09-26 00:41:53 +0800

    nearby

--- weixin/models/weixinLocEvent.php
+++ weixin/models/weixinLocEvent.php
@@ -31,5 +31,21 @@ class weixinLocEvent extends weixinEvent {
 	
         return $this->dbResult($sql);
     }
+
+    public function nearby() {
+
+        $myLat = $this ->latitude;//接收到的当前位置的纬度  
+        $myLng = $this ->longitude;//接收到的当前位置的经度  
+        
+        //以下为核心代码  
+        $range = 180 / pi() * 1 / 6372.797;     //里面的 1 就代表搜索 1km 之内，单位km  
+        $lngR = $range / cos($myLat * pi() / 180);  
+        
+        $maxLat = $myLat + $range;//最大纬度  
+        $minLat = $myLat - $range;//最小纬度  
+        $maxLng = $myLng + $lngR;//最大经度  
+        $minLng = $myLng - $lngR;//最小经度  
+        //得出这四个值以后，就可以根据你数据库里存的经纬度信息查找记录了~  
+    }
 }
 ?>
